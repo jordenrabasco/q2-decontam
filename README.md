f22076a6ac4bcdf052c76623d381a31fd08f6b0c
jordenrabasco <70723193+jordenrabasco@users.noreply.github.com>
2022-09-22 08:17:08 -0400

    file set up
    -initial commit with bare minimum

---
+++ README.md
@@ -0,0 +1,5 @@
+# q2-decontam
+
+![](https://github.com/qiime2/q2-dada2/workflows/ci/badge.svg)
+
+This is a QIIME 2 plugin. For details on QIIME 2, see https://qiime2.org.
---
+++ setup.py
@@ -0,0 +1,33 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2022, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+setup(
+    name="q2-dectontam",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url="https://qiime2.org",
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    author="Jorden Rabasco and Benjamin Callahan",
+    author_email="jrabasc@ncsu.com",
+    description="Apply decontam to present or remove potential contmaination ASVs. ",
+    scripts=['q2_decontam/assets/run_decontam.R'],
+    package_data={
+        'q2_decontam': ['citations.bib']
+    },
+    entry_points={
+        "qiime2.plugins":
+        ["q2-decontam=q2_decontam.plugin_setup:plugin"]
+    },
+    zip_safe=False,
+)
